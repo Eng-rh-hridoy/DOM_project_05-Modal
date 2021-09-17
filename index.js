b2b6aac14cf5ff2eb728f561068d44482f939892
Md. Rakibul Hasan <rhridoy.cse@gmail.com>
2021-09-17 10:14:31 +0600

    every things looks good...and done the project

--- index.js
+++ index.js
@@ -1,11 +1,15 @@
 // Initialize
 const openModalContainer = document.querySelector(".open-modal-container");
-const modalContainer = document.querySelector(".modal-container");
+const modalContainer = document.querySelector(".modal-wrapper");
 const openBtn = document.querySelector(".opn-btn");
+const closeBtn = document.querySelector(".close-modal")
 
 openBtn.addEventListener("click", function(){
-    openModalContainer.style.display = "none";
     modalContainer.style.display = "flex";
-    document.body.style.background = "none";
-    document.body.style.backgroundColor = "blue";
+    document.body.style.backgroundImage = 'linear-gradient(rgb(31 193 255 / 40%), rgb(17 154 206 / 50%)), url(images/background.jpg)';
+});
+
+closeBtn.addEventListener("click", function(){
+    modalContainer.style.display = "none";
+    document.body.style.backgroundImage = 'url("images/background.jpg")';
 });
